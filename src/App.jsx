a053482e347e782e295728c79a6722cec186bf1b
Mobarak Hossain <mobarakhossain121838@gmail.com>
2024-05-28 23:08:05 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className='text-4xl bg-teal-300'>knowledge Cafe</h1>
+     
+    </>
+  )
+}
+
+export default App
